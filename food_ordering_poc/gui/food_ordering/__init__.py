fb699f663a391f35cf935cd4b006a69b087a3fbf
Pavel Pristupa <pristupa@gmail.com>
2020-02-24 16:04:39 +0700

    Extract food-ordering-demo

--- food_ordering_poc/gui/food_ordering/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'food_ordering.apps.FoodOrderingApplication'
--- food_ordering_poc/gui/food_ordering/urls.py
+++
@@ -1,7 +0,0 @@
-from winter.django import create_django_urls
-
-from .controllers.food_ordering_controller import FoodOrderingController
-
-urlpatterns = [
-    *create_django_urls(FoodOrderingController),
-]
